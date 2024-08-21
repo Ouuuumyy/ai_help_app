a31e5ae4d92063d0b4f59c7a23f50e732247766d
oumaima <kadir.oumaima2001@gmail.com>
2024-08-21 21:31:56 +0100

    mod

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -16,12 +16,12 @@ const Header = () => {
     <header className="text-white flex flex-col relative after:content-[''] after:bg-grad-theme-135 after:absolute xl:after:w-[520px] xl:after:h-[520px] md:after:w-[300px] md:after:h-[300px] after:w-[200px] after:h-[200px] after:top-[90%] after:left-0 after:blur-[70px] after:rounded-full before:content-[''] before:bg-grad-theme-135 before:absolute xl:before:w-[520px] xl:before:h-[520px] md:before:w-[300px] md:before:h-[300px] before:w-[200px] before:h-[200px] before:top-[60%] before:right-0 before:blur-[70px] before:rounded-full">
       <div className="max-w-[1320px] px-4 flex items-center justify-between py-[30px] mx-auto w-full relative">
         <div className="flex items-center gap-x-9">
-          <Link
+          {/* <Link
             to={routesConstants.ROOT}
             className="max-w-[120px] lg:max-w-[160px] md:max-w-[140px]"
           >
             <img src={Icons.logo} alt="site logo" />
-          </Link>
+          </Link> */}
           <nav
             className={`lg:relative lg:right-auto lg:top-auto lg:bg-transparent lg:p-0 fixed right-0 top-0 bg-errie-black h-full z-50 p-5 sm:w-[300px] w-full translate-alls ease-in-out duration-300 shadow-navbar lg:shadow-none lg:translate-x-0 ${
               isNavbarOpen ? "translate-x-0" : "translate-x-full"
@@ -38,7 +38,15 @@ const Header = () => {
               <li>
                 <Link
                   to=""
-                  className="text-white text-base font-source-sans hover:opacity-90 transition-all ease-in-out duration-300 font-normal whitespace-nowrap"
+                  className="text-white text-2xl gradi-theme-text font-bold font-source-sans hover:opacity-90 transition-all ease-in-out duration-300  whitespace-nowrap"
+                >
+                  OUM AI
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to=""
+                  className="text-white  text-base font-source-sans hover:opacity-90 transition-all ease-in-out duration-300 font-normal whitespace-nowrap"
                 >
                   Features
                 </Link>
